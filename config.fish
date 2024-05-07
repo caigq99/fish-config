8b0c1ab46862d16c4e44129d749888035e46367e
Cai GuoQing <80808902+caigq99@users.noreply.github.com>
2024-05-07 09:21:06 +0800

    Update config.fish

--- config.fish
+++ config.fish
@@ -2,13 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-test -s /home/caigq/.nvm-fish/nvm.fish; and source /home/caigq/.nvm-fish/nvm.fish
-
 set -gx PATH "$HOME/.npm/node_global/bin" $PATH
 
 function fish_greeting
 
 end
-
-nvm use 20
-clear
